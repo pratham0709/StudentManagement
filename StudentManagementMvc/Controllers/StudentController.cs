267d0feb265cedcf0b630a6e945c0805b00e8e5f
Prathamesh Jadhav <109234076+pratham0709@users.noreply.github.com>
2024-10-18 18:15:27 +0530

    Add files via upload

---
+++ StudentManagementMvc/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentManagementLibrary;
+
+public class StudentController : Controller
+{
+    private readonly IStudentService mystudentService;
+
+    public StudentController(IStudentService studentService)
+    {
+        mystudentService = studentService;
+    }
+
+
+    public IActionResult Index(string firstName = null, string lastName = null, int? age = null)
+    {
+        var students = mystudentService.SearchStudent(firstName, lastName, age);
+        return View(students);
+    }
+
+    [HttpPost]
+    public IActionResult Add(Student student)
+    {
+        mystudentService.AddStudent(student);
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public IActionResult Delete(string firstName, string lastName)
+    {
+        mystudentService.DeleteStudent(firstName, lastName);
+        return RedirectToAction("Index");
+    }
+
+}
